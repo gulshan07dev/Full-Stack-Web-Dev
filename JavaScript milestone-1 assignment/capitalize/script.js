a61c2585ea58d479a20eef27dfb906aca3b17af9
Gulshan kumar <gulshanku9135@gmail.com>
2023-03-06 22:02:12 +0530

    capitalize update

--- JavaScript milestone-1 assignment/capitalize/script.js
+++ JavaScript milestone-1 assignment/capitalize/script.js
@@ -1,9 +1,6 @@
 function checkCapitalornot() {
-  const firstName = document.getElementById("first-name").value;
-  const lastName = document.getElementById("last-name").value;
-
-  firstName[0] === firstName[0].toUpperCase() &&
-  lastName[0] === lastName[0].toUpperCase()
-    ? alert("your name is capitalize")
-    : alert("your name is not capitalize");
-};
+  const firstName = document.getElementById("user-name").value;
+  const result = document.getElementById("result");
+  result.innerText = firstName.charAt(0).toUpperCase() + firstName.slice(1);
+  console.log(result.innerText);
+  };
