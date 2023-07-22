ca0e309072e1118172e2b17711fcf0f9f098818b
gulshan07dev <gulshanku9135@gmail.com>
2023-07-22 10:29:36 +0530

    add react.js assignment

---
+++ React-js Assignment/src/components/List.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function List({ items }) {
+  return (
+    <ul>
+      {items.map((el) => (
+        <li key={el}>{el}</li>
+      ))}
+    </ul>
+  );
+}
